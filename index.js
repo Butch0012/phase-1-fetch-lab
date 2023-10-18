c980616470f6fcf6e22bae7acdb08b5f9c67cc58
Elnathan Mburu <elnathanmburu@gmail.com>
2023-10-18 12:12:35 +0300

    easy

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function fetchBooks() {
+
   // To pass the tests, don't forget to return your fetch!
-  
-}
+    fetch('https://anapioficeandfire.com/api/books')
+      .then(function (response) {return response.json()}) // Convert the response to JSON
+      .then(function(jsInfo) {
+        // Calling the renderBooks function and pass the jsInfo
+        renderBooks(jsInfo);
+      })
+      .catch(function(error){
+        console.error('Error fetching data:', error);
+      });
+  }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
